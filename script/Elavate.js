2f5c81e5f43515e9fc5d7d677f90a24a1a840012
Rachel O <roconnellvisualartist@gmail.com>
2023-09-16 13:10:12 -0400

    added navigation back to index

--- script/Elavate.js
+++ script/Elavate.js
@@ -1,2 +1,29 @@
-alert('Meoww Meow!');
-alert('Tranlation: Welcome to our home!');
+var alertedCat = false;
+var alerted = false;
+
+function welcomeMessage (alertedCat) {
+     if (alertedCat === false) {
+       alert ('Meoww Meow!')
+        alertedCat = true;
+     //   return alert('Meoww Meow!');
+   }
+   else if (alertedCat === true){
+        alertedCat = false
+   }
+}
+
+function translation (alerted) {
+    if (alerted === false) {
+      
+       alerted = true;
+       return alert('Tranlation: Welcome to our home!');
+  }
+}
+
+//<script type="text/javascript">
+//    var alerted = localStorage.getItem('alerted') || '';
+//    if (alerted != 'yes') {
+//     alert("Meoww Meow!");
+//     localStorage.setItem('alerted','yes');
+//    }
+//</script>
