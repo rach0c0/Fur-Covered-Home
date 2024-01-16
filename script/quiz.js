d7e7da0d78c5d490760e4f1d81f52bae6f226d28
Rachel O <roconnellvisualartist@gmail.com>
2024-01-16 00:15:23 -0500

    quiz building

--- script/quiz.js
+++ script/quiz.js
@@ -1,91 +1,55 @@
 
 //create functions to cycle through questions once one is answered
-//back button?
+
 
 //build a set or map to collect and store data from answers
 //findResults.map(answersArray[1,2,3,4,5,6])
 
-//find mode of collected data
-
-//use an if, else if, else loop to determine result based on found mode
-
- //print name, picture, and breif description based on result
 
 
-//const backButton = document.getElementById('back-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.querySelector('question')
-const answerButtonsElement = document.querySelector('aswr-btn')
-const answerElement = document.querySelector('aswr')
 
-const storedAnswers = ['0', '0', '0', '0', '0', '0']
+const questionElement = document.getElementById('question');
+const imageElement = document.querySelector('mc-img');
+const answerElement = document.querySelector('aswr');
 
-//let shuffledQuestions, currentQuestionIndex
-let currentQuestionIndex
+const userAnswers = ['0', '0', '0', '0', '0', '0']; //creating answer index
 
-answerButtonsElement.addEventListener('click' () => {
-    currentQuestionIndex++
-    setNextQuestion()
-  } )
+let currentQuestionIndex = 0; //keeps track of users place in quiz
+let quizActive = true; //true until last question is answered
+let storedAnswers = userAnswers; //Holds answers relating to user selection
 
-//  backButton.addEventListener('click', () => {
-//    currentQuestonIndex--
-//    setNextQuestion()
-//  } )
+document.querySelector('aswr-btn').addEventListener('click', () => {
+  currentQuestionIndex++;
+  if (currentQuestionIndex >= 6) {
+    showQuestion(currentQuestionIndex);
+  }    else {
+      showResult(userAnswers);
+    } 
+} );
 
-
-function answerSelected() {
-  currentQuestionIndex = currentQuestionIndex++
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-    }
-
-//function startGame() {
-  //startButton.classList.add('hide')
-  //shuffledQuestions = questions.sort(() => Math.random() - .5)
-//  currentQuestionIndex = 0
-//  questionContainerElement.classList.remove('hide')
-//  setNextQuestion()
-//}
-
-function setNextQuestion() {
-   // resetState()
-    showQuestion(currentQuestionIndex)
-    }
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answers => {
-    const button = document.createElement('button')
-    button.innerText = answers.innerText
-    //button.classList.add('btn')
-    button.dataset.aswr = answers.aswr
-    button.addEventListener('click' , selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
+//function setNextQuestion() {
+  //  resetState()
+//    showQuestion(currentQuestionIndex)
+//    };
 
 //function resetState() {
 //  clearStatusClass(document.body)
-//  backButton.classList.add('hide')
 //  while (answerButtonsElement.fisrtChild) {
 //    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
 //  }
-//}
+//};
 
-function SelectAnswer(e) {
-  const selectedButton = e.target
-  const aswr = selectedButton.dataset.aswr
-  setStatusClass(document.body, aswr)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  } )
-  if ( ________ > currentQuestionIndex + 1) {
+function showQuestion(question) {
+  questionElement.innerHTML = question.question
+  question.answers.forEach(answers => {
+    answerElement.innerHTML = answers.text
+    imageElement.innerHTML = answers.Image
+  })
+};
 
-  } else {
-    
-  }
-}
+//find mode of collected data
+
+//use an if, else if, else loop to determine result based on found mode
 
 function findResults() {
 /*if mode = 1{
@@ -100,55 +64,61 @@ function findResults() {
     else mode = 4{
         return You are not a cat(Axl)
     }*/
-    }
+    };
 
 //print name, picture, and breif description based on result
 
-const questions = [
+function showResult(){
+
+};
+
+//-----------------Questions & Answer blocks------------------------------------------------
+
+const quizContent = [
     {
      question: 'To get attention you...?',
       answers: [
-        { Image1: './quiz-imgs/cat-swinging', /*correct: true*/ },  //swing from chandalier(slayer)
-        { Image2: './quiz-imgs/cat-speaking-directly', /*correct: false*/ }, //speak directly(Mance)
-        { Image3: './quiz-imgs/cat-sitting-on-target', /*correct: false*/ }, //sit directly on target(Walter)
-        { Image4: './quiz-imgs/cat-nudging-target', /*correct: false*/ }  //poke and nudge target(Axl)
+        { Image: './quiz-imgs/cat-swinging', text: 'Swing from chandalier' }, //(slayer)
+        { Image: './quiz-imgs/cat-speaking-directly', text: 'Speak directly' }, //(Mance)
+        { Image: './quiz-imgs/cat-sitting-on-target', text: 'Sit directly on target' }, //(Walter)
+        { Image: './quiz-imgs/cat-nudging-target', text: 'Poke and nudge target' }, //(Axl)
       ]
     },
     {
         question: 'How would you rather spend your day?',
          answers: [
-           { Image1: './quiz-imgs/cat-in-own-bubble', /*correct: true*/ },  //staying in your happy little bubble(Slayer)
-           { Image2: './quiz-imgs/cat-taking-care-of-others', /*correct: false*/ }, //Making sure others are taken care of(Mance)
-           { Image3: './quiz-imgs/cat-lounging-cuddling', /*correct: false*/ }, //lounging around receiving cuddles(Walter)
-           { Image4: './quiz-imgs/cat-playing-games', /*correct: false*/ }  //playing games/doing activities(Axl)
+           { Image: './quiz-imgs/cat-in-own-bubble', text: 'Staying in your happy little bubble' }, //(Slayer)
+           { Image: './quiz-imgs/cat-taking-care-of-others', text: 'Making sure others are taken care of' }, //(Mance)
+           { Image: './quiz-imgs/cat-lounging-cuddling', text: 'Lounging around receiving cuddles' }, //(Walter)
+           { Image: './quiz-imgs/cat-playing-games', text: 'Playing games/doing activities' }, //(Axl)
          ]
        },
        {
         question: 'You identify as an...?',
          answers: [
-           { Image1: './quiz-imgs/cat-introvert', /*correct: true*/ },  //introvert(Slayer)
-           { Image2: './quiz-imgs/cat-extrovert', /*correct: false*/ }, //extrovert(Walter)
-           { Image3: './quiz-imgs/cat-ambiavert', /*correct: false*/ }, //ambiavert(Mance)
-           { Image4: './quiz-imgs/cat-omnivert', /*correct: false*/ }  //omnivert(Axl)
+           { Image: './quiz-imgs/cat-introvert', text: 'Introvert' }, //(Slayer)
+           { Image: './quiz-imgs/cat-extrovert', text: 'Extrovert' }, //(Walter)
+           { Image: './quiz-imgs/cat-ambiavert', text: 'Ambiavert' }, //(Mance)
+           { Image: './quiz-imgs/cat-omnivert', text: 'Omnivert' }, //(Axl)
          ]
        },
        {
         question: 'What is your love language?',
          answers: [
-           { Image1: './quiz-imgs/cat-act-of-service', /*correct: true*/ },  //Acts of Service(Slayer)
-           { Image2: './quiz-imgs/cat-giving-gifts', /*correct: false*/ }, //giving/receiving gifts(Mance)
-           { Image3: './quiz-imgs/cat-physical-touch', /*correct: false*/ }, //pysical touch(walter)
-           { Image4: './quiz-imgs/cat-quality-time', /*correct: false*/ }  //quality time/words of affrimation(Axl)
+           { Image: './quiz-imgs/cat-act-of-service', text: 'Acts of service' }, //(Slayer)
+           { Image: './quiz-imgs/cat-giving-gifts', text: 'Giving/Receiving gifts' }, //(Mance)
+           { Image: './quiz-imgs/cat-physical-touch', text: 'Pysical touch' }, //(walter)
+           { Image: './quiz-imgs/cat-quality-time', text: 'Quality time/Words of affrimation' }, //(Axl)
          ]
        },
        {
         question: 'What is your favorite genre of music?',
          answers: [
-           { Image1: './quiz-imgs/punk-rock-cat', /*correct: true*/ },  //rock/punk/metal(Slayer)
-           { Image2: './quiz-imgs/classic-music-cat', /*correct: false*/ }, //classical/instrumental/jazz(Mance)
-           { Image3: './quiz-imgs/rnb-rap-cat', /*correct: false*/ }, //R&B/Hip Hop/Pop(Walter)
-           { Image4: './quiz-imgs/country-cat', /*correct: false*/ }  //country(Axl)
+           { Image: './quiz-imgs/punk-rock-cat', text: 'Rock/Punk/Metal' }, //(Slayer)
+           { Image: './quiz-imgs/classic-music-cat', text: 'Classical/Instrumental/Jazz' }, //(Mance)
+           { Image: './quiz-imgs/rnb-rap-cat', text: 'R&B/Hiphop/Pop' }, //(Walter)
+           { Image: './quiz-imgs/country-cat', text: 'Country' }, //(Axl)
          ]
        }
                    
-  ]
+  ];
